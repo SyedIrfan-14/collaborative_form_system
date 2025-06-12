c8688f21d672729cf37fa77e6139914b294ec93f
Syed Irfan  S R <123661933+SyedIrfan-14@users.noreply.github.com>
2025-06-12 09:42:19 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,23 @@
+// db.js
 const mysql = require('mysql2/promise');
 require('dotenv').config();
+const url = require('url');
+
+const dbUrl = process.env.MYSQL_URL;
+
+const { hostname: host, port, pathname, auth } = new URL(dbUrl);
+const [user, password] = auth.split(':');
+const database = pathname.slice(1); // remove leading /
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host,
+  user,
+  password,
+  database,
+  port,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = db;
