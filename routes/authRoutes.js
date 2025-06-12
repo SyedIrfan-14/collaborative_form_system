d732c10c7a003b4e1b08c598f173eaf8408f642b
Syed Irfan  S R <123661933+SyedIrfan-14@users.noreply.github.com>
2025-06-12 12:29:26 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const { body } = require('express-validator');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
 
 router.get('/register', (req, res) => res.render('register'));
 router.get('/login', (req, res) => res.render('login'));
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register',
+  body('email').isEmail(),
+  body('password').isLength({ min: 6 }),
+  register
+);
+router.post('/login',
+  body('email').isEmail(),
+  body('password').notEmpty(),
+  login
+);
 
 module.exports = router;
